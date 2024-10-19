87cfdbaa86b215ab455018282aaeefe5f565c9ab
buingocchinh <149220854+buingocchinh@users.noreply.github.com>
2024-10-19 14:19:58 +0700

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,4 @@
+document.getElementById('confess-button').addEventListener('click', function() {
+    const confession = "Em chọn yêu a làm đúng rồi , giờ gửi ảnh sexy đây =))))";
+    alert(confession);
+});
