6427684668be72b19e42cd0a2f95bef2884e6910
buingocchinh <149220854+buingocchinh@users.noreply.github.com>
2024-10-19 14:27:01 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 document.getElementById('confess-button').addEventListener('click', function() {
     const confession = "Em chọn yêu a làm đúng rồi , giờ gửi ảnh sexy đây =))))";
-    alert(confession);
-});
+    alert(confession)
 alert("Hello bé iu của a><"); // Hiển thị thông báo
+    });
